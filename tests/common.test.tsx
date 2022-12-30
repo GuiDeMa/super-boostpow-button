201558f1ad41e638a40cf04d958b791ed2b39438
Guillaume De Martino <guillaumedemartino@gmail.com>
2022-12-30 10:58:26 +0100

    fix last commit

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
+/*import * as React from 'react'
 import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
 import { BoostButton } from '../src'
 
-/* describe('Common render', () => {
+describe('Common render', () => {
   it('renders without crashing', () => {
     render(<BoostButton content='test' value={0} />)
   })
